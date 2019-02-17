3e888ab48d912f946a3d593bb301481bb887f2b0
Connor Mesec <connor.mesec@workiva.com>
2019-02-17 12:25:15 -0600

    Chart changes

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-var parsedJSON = require('./test.json');
-
-let obj = [JSON.parse(parsedJSON)];
+var fs = require("fs");
+const json = fs.readFileSync('test.json');
+let obj = JSON.parse(json);
 console.log(obj);
+let homeScores = obj[0];
+let awayScores = obj[1];
+let shots = obj[2];
+
+function charts() {
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      [5, 2, 4, 2, 0]
+    ]
+  };
+  
+  // As options we currently only set a static size of 300x200 px. We can also omit this and use aspect ratio containers
+  // as you saw in the previous example
+  var options = {
+    width: 300,
+    height: 200
+  };
+  
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object. As a third parameter we pass in our custom options.
+  new Chartist.Line('.ct-chart', data, options);
+}
