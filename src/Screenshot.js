26f31840f9fd64825934bc43c4f0760f103e3996
Connor Mesec <connor.mesec@workiva.com>
2019-04-29 17:13:52 -0500

    fixed white line issue

--- src/Screenshot.js
+++ src/Screenshot.js
@@ -7,11 +7,23 @@ function Screenshot(props) {
     
     return (
         <button onClick={() => {
-            html2canvas(document.querySelector("#root")).then(canvas => {
-                document.body.appendChild(canvas)
+            html2canvas(document.querySelector(".App")).then(canvas => {
+                document.body.appendChild(canvas);
+                // var download = document.getElementById("download");
+                // var img = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+                // download.setAttribute("href", img);
             });
         }}> Enter </button>
     )
     //React.createElement('h1', {}, data.awayTeam + " at " + data.homeTeam);
   }
+
+  function download() {
+    var download = document.getElementById("download");
+    var image = document.getElementById("myCanvas").toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+    download.setAttribute("href", image);
+    //download.setAttribute("download","archive.png");
+    }
+
   export default Screenshot;
